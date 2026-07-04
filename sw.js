7dfcf8f2e2e8dd36680a6d0d469433483f1c3d46
cichlid528 <168060804+cichlid528@users.noreply.github.com>
2026-07-05 01:12:13 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "bass-spot-log-v28";
+const CACHE_NAME = "bass-spot-log-v29";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./style.css?v=28",
-  "./app.js?v=28",
+  "./style.css?v=29",
+  "./app.js?v=29",
   "./manifest.json",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
