957a02e2d6b7b1eda96b0b24b56e7e97c62d8db6
cichlid528 <168060804+cichlid528@users.noreply.github.com>
2026-07-11 05:21:15 +0900

    釣果詳細に削除ボタンを追加

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "v179-iphone-small-nyan-ui";
+const VERSION = "v181-catch-detail-delete";
 self.addEventListener("install", (event) => { self.skipWaiting(); });
 self.addEventListener("activate", (event) => { event.waitUntil(self.clients.claim()); });
 self.addEventListener("fetch", (event) => { event.respondWith(fetch(event.request)); });
